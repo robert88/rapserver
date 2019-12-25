b4a9bfea37236a33396b4753e88b0c6464790964
robert88 <841354162@qq.com>
2019-12-25 15:42:23 +0800

    Add files via upload

--- aPermission/global.js
+++ aPermission/global.js
@@ -1,1 +1,7 @@
-
+var globalAction = require("@/action/global.js");
+var {definer:d,del,add} = require("@/core/rap.permission.js");
+var promission = {};
+// for(var key in globalAction) {
+//     promission[key] = [d.ACCESS_BY_SESSION];//全部需要登录才可以访问
+// }
+// exports  = module.exports = promission
